b9e3d1ef43b0a6fdfe83a656316671d517a575ae
cmnwankw-cmu-F24 <cmnwankw@andrew.cmu.edu>
2024-11-11 06:54:02 +0200

    added new schema and migration for radiologistinterpretation, aiinterpretation, and doctorscomment

---
+++ src/database/models/AIInterpretation.js
@@ -0,0 +1,30 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const AIInterpretation = sequelize.define(
+    "AIInterpretation",
+    {
+      ai_interpretation_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      study_id: { type: DataTypes.STRING, allowNull: false },
+      diagnosis: { type: DataTypes.TEXT },
+      timestamp: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+      confidence_score: { type: DataTypes.FLOAT },
+    },
+    {
+      timestamps: false,
+      tableName: "AIInterpretation",
+    }
+  );
+
+  //   AIInterpretation.associate = (models) => {
+  //     AIInterpretation.belongsTo(models.Study, {
+  //       foreignKey: "study_id",
+  //       as: "study",
+  //     });
+  //   };
+
+  return AIInterpretation;
+};
